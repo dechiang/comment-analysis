5e5c097084e4bb781e41b5d98ce025afe91bc5da
Dennis Chiang <dennis8654@gmail.com>
2022-01-08 19:33:34 +0800

    calculate len of comment

--- reviewcomment.py
+++ reviewcomment.py
@@ -6,9 +6,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 1000 == 0:
+		if count % 1000 == 0: # 如果count 的餘數和 1000 是 0
 			print(len(data))
-print(len(data))
-print(data[0])
-print('-------------')
-print(data[1])
+print('the file has been uploaded succussfully, it has total: ', len(data), 'comments')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) # sum = sum + len(d)
+print('the average length of each comment: ', sum_len / len(data)) 
